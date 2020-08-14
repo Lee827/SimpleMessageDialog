66d11455bd49ee152c0dcb854da5d14bd29c18f3
Elmo Lee <elmolee@x-nb.web-on.org>
2020-08-14 12:40:19 +0800

    Create Framework

---
+++ SimpleMessageDialog/SimpleMessageDialog/SimpleMessageDialog.h
@@ -0,0 +1,19 @@
+//
+//  SimpleMessageDialog.h
+//  SimpleMessageDialog
+//
+//  Created by Elmo Lee on 14/8/2020.
+//  Copyright © 2020 ElmoLee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SimpleMessageDialog.
+FOUNDATION_EXPORT double SimpleMessageDialogVersionNumber;
+
+//! Project version string for SimpleMessageDialog.
+FOUNDATION_EXPORT const unsigned char SimpleMessageDialogVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleMessageDialog/PublicHeader.h>
+
+
